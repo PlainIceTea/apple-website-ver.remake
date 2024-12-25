9f1ed0c59201f46ed11e8fc28a2648cf0bf68a43
unknown <astisyaf@gmail.com>
2024-12-25 15:39:08 +0700

    first commit and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
+const App = () => {
+  return (
+    <main className="bg-black text-white min-h-screen">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
